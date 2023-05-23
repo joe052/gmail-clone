8c60a0e884002657a0e63d0e9780faa8a4a1c5bd
joe052 <joekaranjasenior52@gmail.com>
2023-05-23 03:39:55 +0300

    message

--- src/views/template/EmailView.vue
+++ src/views/template/EmailView.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="EmailView" class="bg-gray-50 h-screen">
     <div class="flex items-center p-2">
-      <div class="flex justify-center items-center px-3.5"></div>
+      <div class="flex justify-center items-center px-3.5">
+        <IconComponent iconString="menu" iconColor="#636363" :iconSize="19" />
+      </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import IconComponent from "@/components/IconComponent.vue";
+</script>
