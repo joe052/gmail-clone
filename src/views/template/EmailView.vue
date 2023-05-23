f6cc35f6906ef8c756d23e647dfd423e97ca4480
joe052 <joekaranjasenior52@gmail.com>
2023-05-23 03:11:12 +0300

    message

--- src/views/template/EmailView.vue
+++ src/views/template/EmailView.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="EmailView" class="bg-gray-50 h-screen">
-    <div class="flex items-center p-2"></div>
+    <div class="flex items-center p-2">
+      <div class="flex justify-center items-center px-3.5"></div>
+    </div>
   </div>
 </template>
