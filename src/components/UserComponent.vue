1de569e828ecb21431017cc473d4115e3cbff00d
joe052 <joekaranjasenior52@gmail.com>
2023-05-28 23:11:42 +0300

    message

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,5 +1,10 @@
 <template>
-    <div id="UserComponent">
-        <img src="https://via.placeholder.com/40">
-    </div>
+  <div id="UserComponent">
+    <img
+      data-tooltip-target="`tooltip-no-arrow-user`"
+      data-tooltip-placement="bottom"
+      src="https://via.placeholder.com/40"
+      
+    />
+  </div>
 </template>
